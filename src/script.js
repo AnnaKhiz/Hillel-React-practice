f5b8ff4a15216ce1fbeaee2ced4e2074da48f03a
AnnaKhiz <kukurika.anna@gmail.com>
2023-12-15 14:03:06 +0200

    add back button

--- src/script.js
+++ src/script.js
@@ -1,23 +1,26 @@
 import './sass/style.sass';
 import React, {useEffect} from "react";
 import ReactDOM from 'react-dom';
+import store from "./store";
 import {Provider, useSelector, useDispatch} from 'react-redux';
 import {MainContainer} from "./components/MainContainer.jsx";
 import {LoginForm} from "./components/LoginForm.jsx";
-import store from "./store";
+import { backToPortfolio } from "./js/back.js";
 import {getFetchUserList} from './actions';
 
 const App = () => {
 	const dispatch = useDispatch();
 	const pageCount = useSelector(state => state.calculationReducer.count);
 
+	useEffect(backToPortfolio, []);
+
 	useEffect(() => {
 		dispatch(getFetchUserList(pageCount))
-	}, [pageCount])
-
+	}, [pageCount]);
 
 	return (
 		<>
+			<div className="button-back" id="button-back"></div>
 			<h1 className={"header"}>User list</h1>
 			{localStorage.getItem('token') ?
 				<MainContainer /> :
